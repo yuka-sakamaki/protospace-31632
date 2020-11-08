c3d90b2f2f1a6426ed8285b7bc181e3206827cdc
super-sausage <69895997+super-sausage@users.noreply.github.com>
2020-11-09 08:48:20 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,63 @@
+class PrototypesController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
+  def index
+    @prototype = Prototype.includes(:user).order("created_at DESC")
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.create(prototype_params)
+    #Rails.logger.debug "@prototype : #{@prototype.inspect}"
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    @prototype.update(prototype_params)
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    if prototype.destroy
+      redirect_to root_path
+    end
+  end
+
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id:current_user.id)
+  end
+
+  def move_to_index
+    unless  user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+end
